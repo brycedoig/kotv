0cd50830a5a9b839a68a0894f5e6715cd9d0159d
brycedoig <brycedoig@gmail.com>
2014-03-15 20:32:34 -0700

    Small Change!

--- Unity Project/Assets/Scripts/FollowCamera.cs
+++ Unity Project/Assets/Scripts/FollowCamera.cs
@@ -8,8 +8,6 @@ public class FollowCamera : MonoBehaviour {
 	public float velocityLerpScale = 0.5f;
 	public Vector3 baseOffset = Vector3.zero;
 	private Vector3 velocityOffset = Vector3.zero;
-	public float maxXVelocityOffset = 1f;
-	public float maxYVelocityOffset = 2f;
 	private GameObject followObject = null;
 	// Use this for initialization
 	void Start () 
@@ -22,7 +20,6 @@ public class FollowCamera : MonoBehaviour {
 	void GetVelocityOffset()
 	{
 		Vector2 vel = followObject.rigidbody2D.velocity;
-		float maxSpeed = followObject.GetComponent<PlayerController>().maxXSpeed;
 		Vector3 targetOffset = new Vector3(vel.x, vel.y, 0f) * velocityLerpScale;
 
 		velocityOffset = Vector3.Lerp(velocityOffset, targetOffset, Time.fixedDeltaTime * velocityLerpSpeed);
