b25438557c73c1e5242e6c2be9a2fd3af4053ce9
brycedoig <brycedoig@gmail.com>
2014-03-15 20:26:07 -0700

    Camera Changes

--- Unity Project/Assets/Scripts/FollowCamera.cs
+++ Unity Project/Assets/Scripts/FollowCamera.cs
@@ -3,8 +3,13 @@ using System.Collections;
 
 public class FollowCamera : MonoBehaviour {
 
-	public Vector3 followOffset = Vector3.zero;
-	public float speed = 4f;
+	public float baseLerpSpeed = 4f;
+	public float velocityLerpSpeed = 4f;
+	public float velocityLerpScale = 0.5f;
+	public Vector3 baseOffset = Vector3.zero;
+	private Vector3 velocityOffset = Vector3.zero;
+	public float maxXVelocityOffset = 1f;
+	public float maxYVelocityOffset = 2f;
 	private GameObject followObject = null;
 	// Use this for initialization
 	void Start () 
@@ -13,10 +18,20 @@ public class FollowCamera : MonoBehaviour {
 		if(!followObject)
 			Debug.Log("No followObject found!");
 	}
-	
+
+	void GetVelocityOffset()
+	{
+		Vector2 vel = followObject.rigidbody2D.velocity;
+		float maxSpeed = followObject.GetComponent<PlayerController>().maxXSpeed;
+		Vector3 targetOffset = new Vector3(vel.x, vel.y, 0f) * velocityLerpScale;
+
+		velocityOffset = Vector3.Lerp(velocityOffset, targetOffset, Time.fixedDeltaTime * velocityLerpSpeed);
+	}
+
 	// Update is called once per frame
 	void FixedUpdate () 
 	{
-		transform.position = Vector3.Lerp(transform.position, followObject.transform.position + followOffset, Time.deltaTime * speed);
+		GetVelocityOffset();
+		transform.position = Vector3.Lerp(transform.position, followObject.transform.position + baseOffset + velocityOffset, Time.fixedDeltaTime * baseLerpSpeed);
 	}
 }
